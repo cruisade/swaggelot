bcceb882b71af4f5bb2f841e83d510f6080835da
cruisade <enoughnight@gmail.com>
2020-05-27 02:06:52 +0300

    add empty readme

--- SampleOcelot/Program.cs
+++ SampleOcelot/Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Ocelot.DependencyInjection;
 using Swaggelot.Configuration;
 
 namespace SampleOcelot
